3052fbff1bb8a2626ccb9668070ca18f485ba21b
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-03-12 17:05:27 +0200

    added form to sign in component

--- backend/frontend/src/Sign_In.js
+++ backend/frontend/src/Sign_In.js
@@ -1,12 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
-function Sign_in(props){
-    return(
-        <div>
-            <h1>This is the sign in component
-            </h1>
-        </div>
-    )
+function Sign_in_form(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function changeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function changePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function submit(e) {
+    e.preventDefault();
+
+    alert(email + password);
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <h3 id="headingSign">Sign In</h3>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form onSubmit={submit}>
+          <Form.Group>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={changeEmail}
+              autoComplete="none"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={changePassword}
+              autoComplete="none"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="submit"
+              value="Submit and Sign In"
+              className="btn btn-success"
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
-export default Sign_in
+function Sign_in(props) {
+  const [modalShow, setModalShow] = useState(false);
+  return (
+    <>
+      <Button
+        className="shadow-lg p-3 m-5 btn-lg"
+        variant="warning"
+        onClick={() => setModalShow(true)}
+        style={{ color: "#AC2020" }}
+      >
+        Sign In
+      </Button>
+
+      <Sign_in_form show={modalShow} onHide={() => setModalShow(false)} />
+    </>
+  );
+}
+
+export default Sign_in;
