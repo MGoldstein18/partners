ac13eca5519315fdb24e55023f5815ae97032b00
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-03-12 16:58:46 +0200

    started creating sign in component

--- backend/frontend/src/Home.js
+++ backend/frontend/src/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import './App.css'
+import "./App.css";
+import Sign_in from "./Sign_In.js";
 
 function Home(props) {
   return (
-    <Jumbotron className="text-center" id="home_heading">
-      <h1>Welcome!</h1>
-      <p>Stay motivated to complete courses with a partner!</p>
-    </Jumbotron>
+    <div>
+      <Jumbotron className="text-center" id="home_heading">
+        <h1>Welcome!</h1>
+        <p>Stay motivated to complete courses with a partner!</p>
+      </Jumbotron>
+      <Sign_in />
+    </div>
   );
 }
 
---
+++ backend/frontend/src/Sign_In.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Sign_in(props){
+    return(
+        <div>
+            <h1>This is the sign in component
+            </h1>
+        </div>
+    )
+}
+
+export default Sign_in
